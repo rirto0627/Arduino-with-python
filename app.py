2f0a492c67cb0e6e6c00064b74d1faac69497519
rirto0627 <47493742+rirto0627@users.noreply.github.com>
2020-05-01 00:09:30 +0800

    fix mysql

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import serial
 import MySQLdb
 
-db  =  MySQLdb . connect (
-     host = "#" ,     #主機名
-     user = "#" ,          #用戶名
-     passwd = "#" ,   #密碼
-     db = "#" )         #數據庫名稱
+#db  =  MySQLdb . connect (
+#     host = "#" ,     #主機名
+#     user = "#" ,          #用戶名
+#     passwd = "#" ,   #密碼
+#     db = "#" )         #數據庫名稱
 
 ser = serial.Serial("COM4", 9600, timeout=1)
 s = ser.read(10)
